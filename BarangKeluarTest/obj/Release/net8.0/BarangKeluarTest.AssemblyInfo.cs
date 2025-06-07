716ef189c3e6b431893655d04ed0e3f0a2295580
mragenks <theqar07@gmail.com>
2025-06-07 11:49:02 +0700

    Fitur: Login, Logout, Lihat Barang (Dummy), Lihat Akun (Super Admin)

--- BarangKeluarTest/obj/Release/net8.0/BarangKeluarTest.AssemblyInfo.cs
+++ BarangKeluarTest/obj/Release/net8.0/BarangKeluarTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarangKeluarTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b57b5848779cf5d371fc1d3b39b56545ce611b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7089bef9c79bc70dbdaf800ce42c2d9d0be8438")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarangKeluarTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarangKeluarTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
